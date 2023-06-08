15bf08a6e69fa1b1388d86459b9d5bc6a9aa984e
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-06-08 18:57:02 +0200

    Add book to ui implemented, defer atted to js link

--- app.js
+++ app.js
@@ -47,6 +47,13 @@ class UI {
 
     list.appendChild(row);
   }
+
+  static clearFields() {
+    document.querySelector('#title').value =
+      document.querySelector('#author').value =
+      document.querySelector('#isbn').value =
+        '';
+  }
 }
 // Store Class: Handle Storage
 
@@ -54,5 +61,23 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Add Book to UI
+  UI.addBookToList(book);
+
+  // Clear fields
+  UI.clearFields();
+});
 
 // Event: Remove a Book
