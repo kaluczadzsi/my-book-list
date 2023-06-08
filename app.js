490fd2f2383f126186f7d6eae9b9e232de03a458
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-06-08 19:07:32 +0200

    Remove function added

--- app.js
+++ app.js
@@ -48,6 +48,12 @@ class UI {
     list.appendChild(row);
   }
 
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
   static clearFields() {
     document.querySelector('#title').value =
       document.querySelector('#author').value =
@@ -81,3 +87,6 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 });
 
 // Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  UI.deleteBook(e.target);
+});
